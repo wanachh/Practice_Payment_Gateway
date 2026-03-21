3521f55633ce9a255bb026407ac79b394f81b854
wanachh <wanachh.kam@gmail.com>
2026-03-21 14:16:15 +0700

    chore: finally ignored .vs folder

---
+++ README.md
@@ -0,0 +1,17 @@
+# Practice_Payment_Gateway
+
+My mock project to understand about payment gateway and the structure
+mostly part of this project I use Gemini to guide and explain code
+
+
+I have 2 solutions in this project
+
+1.ZenPay.Core for logic, models, and data definition.
+
+    - Data/ZenPayDbContext.cs: database bridge.
+
+    - Migrations/: The InitialCreate migration.
+
+    - Models/Transaction.cs: Transaction data definition.
+
+2.ZenPay.Api for outside world requests.
---
+++ ZenPay.Core/Models/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using ZenPay.Core.ENUM;
+
+namespace ZenPay.Core.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(50)]
+        public string TransactionNumber { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string Currency { get; set; } = "THB";
+
+        [Required]
+        public string Status { get; set; } = "Pending";
+
+        public string? ErrorCode { get; set; } //if no error = NULL
+
+        public DateTimeOffset TransactionDateTime { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
