1840e73f8525469f64e308c34b292bba5783ada6
wanachh <wanachh.kam@gmail.com>
2026-03-21 14:11:29 +0700

    test

---
+++ ZenPay.Core/Data/ZenPayDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenPay.Core.Models;
+
+namespace ZenPay.Core.Data;
+
+public class ZenPayDbContext : DbContext
+{
+    public ZenPayDbContext(DbContextOptions<ZenPayDbContext> options) : base(options) { }
+
+    // If you have 'using System.Transactions;' at the top, REMOVE IT.
+    public DbSet<Transaction> Transactions { get; set; }
+}
---
+++ ZenPay.Core/ENUM/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZenPay.Core.ENUM
+{
+    public enum TransactionStatus
+    {
+        Pending,
+        Authorized,
+        Declined,
+        Captured,
+        Refunded,
+        Failed
+    }
+}
